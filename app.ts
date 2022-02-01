933a0e98af6203a5eea36a0b9387bcf46daa6ea0
Luca <95436769+Lukenssss@users.noreply.github.com>
2022-02-01 09:56:14 -0300

    Deleted an unnecessary route :)

--- app.ts
+++ app.ts
@@ -7,14 +7,6 @@ app.get('/', (req, res) => {
     res.send("Hola!")
 })
 
-app.get('/check', (req, res) => {
-    const email = req.body.email
-    const username = req.body.username
-    const password = req.body.password
-
-    
-})
-
 app.listen(PORT, () => {
     console.log(`Listening ${PORT}!`)
 })
